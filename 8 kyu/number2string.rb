ad58d2299cdccd3e65256b9b8fd6f4d256ebac51
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-09-30 19:30:10 +0300

    Add files via upload

---
+++ 8 kyu/number2string.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# We need a function that can transform a number into a string.
+
+# What ways of achieving this do you know?
+
+# Examples:
+# 123 --> "123"
+# 999 --> "999"
+
+def number_to_string(num)
+  num.to_s
+end

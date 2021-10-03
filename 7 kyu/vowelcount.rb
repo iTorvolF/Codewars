f854c740546945c0a849bf8ed331316517e735a6
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-03 16:26:27 +0300

    Add files via upload

---
+++ 7 kyu/vowelcount.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Return the number (count) of vowels in the given string.
+
+# We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+# The input string will only consist of lower case letters and/or spaces.
+
+def getCount(inputStr)
+  inputStr.count('aeiou')
+end

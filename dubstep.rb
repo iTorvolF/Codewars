3b1856c26510e7b5eaad2c09cbbe48890ba972aa
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-17 19:25:41 +0300

    Delete dubstep.rb

--- dubstep.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-song = 'WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB'
-
-def song_decoder(song)
-  song.split('WUB').reject(&:empty?).join(' ')
-end
-
-puts song_decoder(song)

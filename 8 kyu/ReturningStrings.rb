6e52b9a0f2cd74be3d1a8dd6cc09ef9926465c8c
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-24 19:52:58 +0300

    Add files via upload

---
+++ 8 kyu/ReturningStrings.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Make a function that will return a greeting statement that uses an input;
+# your program should return, "Hello, <name> how are you doing today?".
+
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+# my solution:
+
+def greet(name)
+  "Hello, #{name} how are you doing today?"
+end

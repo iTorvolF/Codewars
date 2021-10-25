df73da5ce1f94da4cc5b18f678748702f65f246d
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-10-25 19:52:00 +0300

    Add files via upload

---
+++ 8 kyu/LostWithoutaMap.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+
+# my solution:
+
+def maps(x)
+  x.map { |i| i * 2 }
+end

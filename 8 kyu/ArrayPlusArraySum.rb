aff89616f3b710938d79a35a286dafd0c20a1291
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-02 22:28:38 +0300

    Add files via upload

---
+++ 8 kyu/ArrayPlusArraySum.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+# I'll appreciate for your help.
+
+# P.S. Each array includes only integer numbers. Output is a number too.
+
+# my solution:
+
+def array_plus_array(arr1, arr2)
+  (arr1 + arr2).sum
+end

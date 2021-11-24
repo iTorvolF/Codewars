97c5f49009ff684907663f750eef2586c73d2cd5
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-24 21:53:05 +0300

    Add files via upload

---
+++ 7 kyu/SumNumbers.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Given two integers a and b, which can be positive or negative,
+# find the sum of all the integers between and including them and return it.
+# If the two numbers are equal return a or b.
+
+# NOTE: a and b are not ordered!
+
+# Examples (a, b) --> output (explanation)
+
+# (1, 0) --> 1 (1 + 0 = 1)
+# (1, 2) --> 3 (1 + 2 = 3)
+# (0, 1) --> 1 (0 + 1 = 1)
+# (1, 1) --> 1 (1 since both are same)
+# (-1, 0) --> -1 (-1 + 0 = -1)
+# (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+# my solution:
+
+def get_sum(a, b)
+  Range.new(*[a, b].sort).sum
+end

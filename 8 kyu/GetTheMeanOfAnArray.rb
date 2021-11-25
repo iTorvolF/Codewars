9006ca87264b32c4f9f5bfe624b39794bbb5a7f9
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-25 21:41:02 +0300

    Add files via upload

---
+++ 8 kyu/GetTheMeanOfAnArray.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# It's the academic year's end, fateful moment of your school report. The averages must be calculated.
+# All the students come to you and entreat you to calculate their average for them.
+# Easy ! You just need to write a script.
+
+# Return the average of the given array rounded down to its nearest integer.
+
+# The array will never be empty.
+
+# my solution:
+
+def get_average(marks)
+  marks.inject { |sum, el| sum + el } / marks.size
+end

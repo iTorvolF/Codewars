f5edf0efbfd5f4036b78c53895d808e349280b97
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-27 14:03:15 +0300

    Add files via upload

---
+++ 8 kyu/ConvertBooleanToString.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Implement a function which convert the given boolean value into its string representation.
+
+# NOTE: Only valid inputs will be given.
+
+# my solution:
+
+def boolean_to_string(b)
+  b.to_s
+end

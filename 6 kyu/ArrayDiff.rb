04c2d7af30242a99ae450f4eed939d96130468bd
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-29 19:59:08 +0300

    Add files via upload

---
+++ 6 kyu/ArrayDiff.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Your goal in this kata is to implement a difference function,
+# which subtracts one list from another and returns the result.
+
+# It should remove all values from list a, which are present in list b keeping their order.
+
+# array_diff([1,2],[1]) == [2]
+# If a value is present in b, all of its occurrences must be removed from the other:
+
+# array_diff([1,2],[1]) == [2]
+
+# my solution:
+
+def array_diff(a, b)
+  a.to_a - b.to_a
+end

0582c773a653c456b43b8befcaa1fe498f1866f4
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-11-30 19:31:06 +0300

    Add files via upload

---
+++ 6 kyu/BitCounting.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Write a function that takes an integer as input,
+# and returns the number of bits that are equal to one in the binary representation of that number.
+# You can guarantee that input is non-negative.
+
+# Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+# my solution:
+
+def count_bits(n)
+  n.to_s(2).count '1'
+end
+
+# another solution:
+
+def count_bits(n)
+  n.digits(2).select(&:positive?).count
+end

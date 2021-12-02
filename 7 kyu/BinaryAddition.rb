bbcfa2707bc5485ac22675e9cc12ad8d7b8ce9da
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-02 20:49:07 +0300

    Add files via upload

---
+++ 7 kyu/BinaryAddition.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Implement a function that adds two numbers together and returns their sum in binary.
+# The conversion can be done before, or after the addition.
+
+# The binary number returned should be a string.
+
+# Examples:(Input1, Input2 --> Output (explanation)))
+
+# 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+# 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+# my solution:
+
+def add_binary(a, b)
+  (a + b).to_s(2)
+end

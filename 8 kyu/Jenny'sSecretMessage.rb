22a252719f9c9ec3c0dba385bef8129e2e590149
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2021-12-07 21:44:35 +0300

    Add files via upload

---
+++ 8 kyu/Jenny'sSecretMessage.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Jenny has written a function that returns a greeting for a user.
+# However, she's in love with Johnny, and would like to greet him slightly different.
+# She added a special case to her function, but she made a mistake.
+
+# Can you help her?
+
+# my solution:
+
+def greet(name)
+  return 'Hello, my love!' if name == 'Johnny'
+
+  "Hello, #{name}!"
+end

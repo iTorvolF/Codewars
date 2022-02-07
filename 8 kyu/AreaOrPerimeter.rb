cbf746d8ce507099466d05daf518ba3ddb23ebf9
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2022-02-07 23:22:59 +0300

    Add files via upload

---
+++ 8 kyu/AreaOrPerimeter.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# You are given the length and width of a 4-sided polygon.
+# The polygon can either be a rectangle or a square.
+# If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+# area_or_perimeter(6, 10) --> 32
+# area_or_perimeter(3, 3) --> 9
+# Note: for the purposes of this kata you will assume that it is a square if its length
+# and width are equal, otherwise it is a rectangle.
+
+# my solution:
+
+def area_or_perimeter(l, w)
+  if l == w
+    l * w
+  else
+    (l + w) * 2
+  end
+end
+
+# best solution:
+
+def area_or_perimeter(l, w)
+  l == w ? l * w : 2 * (l + w)
+end

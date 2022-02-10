cbd12a8ff4674b4cb2cd7ce59f24302ec47fce33
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2022-02-10 20:51:46 +0300

    Add files via upload

---
+++ 7 kyu/TwoToOne.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Take 2 strings s1 and s2 including only letters from ato z.
+# Return a new sorted string, the longest possible,
+# containing distinct letters - each taken only once - coming from s1 or s2.
+
+# Examples:
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+
+# a = "abcdefghijklmnopqrstuvwxyz"
+# longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+# my solution:
+
+def longest(a1, a2)
+  (a1 + a2).split('').sort.uniq.join('')
+end

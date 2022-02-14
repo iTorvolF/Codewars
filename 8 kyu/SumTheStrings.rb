a9f027ada8d7730b367353f2b23bd82da132dc86
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2022-02-14 19:23:32 +0300

    Add files via upload

---
+++ 8 kyu/SumTheStrings.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Create a function that takes 2 integers in form of a string as an input,
+# and outputs the sum (also as a string):
+
+# Example: (Input1, Input2 -->Output)
+
+# "4",  "5" --> "9"
+# "34", "5" --> "39"
+# "", "" --> "0"
+# "2", "" --> "2"
+# "-5", "3" --> "-2"
+# Notes:
+
+# If either input is an empty string, consider it as zero.
+
+# Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+# my solution:
+
+def sum_str(a, b)
+  res = a.to_i + b.to_i
+  res.to_s
+end
+
+# best solution:
+
+def sum_str(a, b)
+  (a.to_i + b.to_i).to_s
+end

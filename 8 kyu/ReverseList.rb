2778369428da88843e339ae4e0260af315918b59
iTorvolF <88985284+iTorvolF@users.noreply.github.com>
2022-03-06 06:29:57 +0300

    Add files via upload

---
+++ 8 kyu/ReverseList.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+# Examples (Input -> Output)
+# * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+# * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+# my solution:
+
+def reverse_list(list)
+  list.reverse
+end
